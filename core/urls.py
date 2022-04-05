19760a344ea4d94d8963c4283a962e35f67e5f49
Rowan <rowan@4e76a5ed.net>
2022-04-05 11:17:32 +0100

    add token login view

--- core/urls.py
+++ core/urls.py
@@ -6,5 +6,6 @@ router = DynamicRouter()
 router.register("examples", core_views.ExampleViewSet)
 
 urlpatterns = router.urls + [
+    path("login", core_views.TokenLogin.as_view(), name="login"),
     path("ping", core_views.ping, name="ping"),
 ]
--- core/views.py
+++ core/views.py
@@ -1,11 +1,26 @@
 from dynamic_rest.viewsets import DynamicModelViewSet
 from rest_framework import status
+from rest_framework.authtoken.models import Token
+from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.exceptions import ValidationError
 from rest_framework.response import Response
 
 import core.serializers as core_serializers
 import core.models as core_models
+
+
+class TokenLogin(ObtainAuthToken):
+    authentication_classes = []
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(
+            data=request.data, context={"request": request}
+        )
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data["user"]
+        token, created = Token.objects.get_or_create(user=user)
+        return Response({"token": token.key})
 
 
 class ExampleViewSet(DynamicModelViewSet):
